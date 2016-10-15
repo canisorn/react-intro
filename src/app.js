b3d60563730d94c394b004800e200b75219518f2
canisorn <canisorn-joy@hotmail.com>
2016-10-15 10:40:16 +0700

    update react intro

---
+++ src/app.js
@@ -0,0 +1,12 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title ="My React App"
+
+const App = () => {
+	
+	return <div>My React App </div>
+	
+}
+const element = document.getElementById('app')
+ReactDOM.render(<App / >,element)
