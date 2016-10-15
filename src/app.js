bf4208355fcdf4788b95cf9eca163ab89d625953
canisorn <canisorn-joy@hotmail.com>
2016-10-15 12:02:48 +0700

    Create html and app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,69 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title ="My React App"
 
-const App = () => {
-	
-	return <div>My React App </div>
-	
+
+const Header = (props) =>  (
+	 <header>
+		<h1> {props.title} </h1>
+		<SearhForm />
+	 </header>
+ )
+ //{ ไว้ในจาวาสคริปเข้าไป}
+
+ 
+ const AppWithoutDescription = () =>(
+	<Header  title2="No description here"/>
+  
+ )
+ 
+ const Items = (props)=>{
+	 console.log(props.items)
+	 return (
+		<ul>
+			{
+				props.items.map(items =>(
+					<li>{items}</li>
+				
+				))
+			}
+		</ul>
+		)
 }
+
+ const SearhForm = () => {
+        return (
+
+            <form>
+                <input type="text" />
+                <button type="submit">search</button>
+            </form>
+        )
+  }
+
+const Content = (props) => (
+		<section>
+			<p> {props.contentTxt}</p>
+			 <Items items={props.items}/>
+		</section>
+)
+
+
+const App = () => {	
+	const appTitle ="Frontteachs :React"
+	const contentTxt  = "This is a simple react application"
+	const items =[
+		"Oliver", "Tobey" ,"Charlie" , "Lucky"
+	]
+	
+	return (
+		<section className="app-container">
+			<Header  title={appTitle}/>
+			<Content contentTxt = {contentTxt} items={items}/>
+		</section>
+	 )
+} 
+
 const element = document.getElementById('app')
-ReactDOM.render(<App / >,element)
+ReactDOM.render(<App/ >,element)
+//ReactDOM.render(<AppWithoutDescription / >,element)
